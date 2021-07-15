029f3687b8cf7a5bce366c440aa24279366f3d3f
J.T. Gilkeson <gilkesonconsulting@gmail.com>
2021-07-15 14:57:45 -0700

    Added some data tests

--- app/src/test/java/com/jt/stockquoteshiltmvvm/presentation/MainViewModelTest.kt
+++ app/src/test/java/com/jt/stockquoteshiltmvvm/presentation/MainViewModelTest.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.Observer
 import com.jt.stockquoteshiltmvvm.domain.model.Quote
 import com.jt.stockquoteshiltmvvm.domain.repository.QuoteRepository
 import com.jt.stockquoteshiltmvvm.utils.CoroutinesTestRule
+import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 
@@ -14,7 +15,6 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Mock
 import org.mockito.Mockito.doReturn
 import org.mockito.Mockito.verify
@@ -52,12 +52,17 @@ class MainViewModelTest {
         val testSymbol = "TEST"
         doReturn(Quote(5.0)).`when`(quoteRepository).getQuote(testSymbol)
         val event = QuoteEvent.GetQuoteEvent(testSymbol)
-        viewModel.getLoading().observeForever(observer)
+        val loading = viewModel.getLoading()
+        loading.observeForever(observer)
 
         //When
         viewModel.onTriggerEvent(event)
 
         //Then
         verify(observer).onChanged(true)
+        assertEquals(true, loading.value)
+
+        advanceTimeBy(1000)
+        assertEquals(false, loading.value)
     }
 }
