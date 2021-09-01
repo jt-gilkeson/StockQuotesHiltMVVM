6accef9eec7c49709dd8d63fed1ae4cc7e5adbbe
J.T. Gilkeson <gilkesonconsulting@gmail.com>
2021-09-01 11:56:42 -0700

    Move token into properties file, update gitignore

--- app/src/main/java/com/jt/stockquoteshiltmvvm/data/service/QuoteService.kt
+++ app/src/main/java/com/jt/stockquoteshiltmvvm/data/service/QuoteService.kt
@@ -1,11 +1,12 @@
 package com.jt.stockquoteshiltmvvm.data.service
 
+import com.jt.stockquoteshiltmvvm.BuildConfig
 import com.jt.stockquoteshiltmvvm.data.dto.QuoteDto
 import retrofit2.http.GET
 
 import retrofit2.http.Query
 
 interface QuoteService {
-    @GET("quote?token=c3jqbpiad3i82raokje0")
+    @GET("quote?token=${BuildConfig.TOKEN}")
     suspend fun getQuote(@Query("symbol") symbol: String): QuoteDto
 }
